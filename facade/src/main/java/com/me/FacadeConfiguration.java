3643fff861042dfa1f4e43f12dd0dc1b53d19fac
WangSenyuan <brucewang@maycur.com>
2016-12-07 11:04:41 +0800

    try swagger

--- facade/src/main/java/com/me/FacadeConfiguration.java
+++ facade/src/main/java/com/me/FacadeConfiguration.java
@@ -5,12 +5,18 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Created by wangsenyuan on 7/26/16.
  */
 @ComponentScan
 @Configuration
+@EnableSwagger2
 public class FacadeConfiguration {
 
     @Bean
@@ -19,5 +25,9 @@ public class FacadeConfiguration {
         return new RestTemplate();
     }
 
-
+    @Bean
+    public Docket petApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any()).build();
+    }
 }
