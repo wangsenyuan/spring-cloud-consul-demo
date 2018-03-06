d5b3021bcc025e7670ec717a8255fc4a4fe0f68b
WangSenyuan <brucewang@maycur.com>
2018-03-06 14:24:21 +0800

    aaaa

--- config-server/src/main/java/com/me/ConfigServerApplication.java
+++ config-server/src/main/java/com/me/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.me;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
